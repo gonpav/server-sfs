80e1e42ef3e6b75897184355fe5f139f9c736335
Vladimir Volodin <zgollum@gmail.com>
2012-12-26 12:05:23 +0200

    Installed packages

--- PilotProject/PilotProject/Global.asax.cs
+++ PilotProject/PilotProject/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace PilotProject
 {
@@ -16,6 +17,8 @@ namespace PilotProject
     {
         protected void Application_Start()
         {
+            // Register the default hubs route: ~/signalr/hubs
+            RouteTable.Routes.MapHubs();
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
