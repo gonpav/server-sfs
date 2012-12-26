740fa96f855fe79f7034e81a74f8fcd135f9cc28
Vladimir Volodin <zgollum@gmail.com>
2012-12-26 16:01:01 +0200

    MessageHub

---
+++ PilotProject/PilotProject/SignalR/MessageHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR;
+using PilotProject.Models;
+
+namespace PilotProject.SignalR
+{
+    public class MessageHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+        public void SubToMedic(int medicId)
+        {
+            System.Diagnostics.Trace.WriteLine("Test" + medicId.ToString());
+            System.Diagnostics.Trace.WriteLine(Context.ConnectionId);
+            Groups.Add(Context.ConnectionId, medicId.ToString());
+        }
+        public static void SendMedicUpdate(Medic medic)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<MessageHub>();
+            System.Diagnostics.Trace.WriteLine("Sending medic update");
+            context.Clients.Group(medic.medicID.ToString()).updateMedic(medic);
+        }
+    }
+}
