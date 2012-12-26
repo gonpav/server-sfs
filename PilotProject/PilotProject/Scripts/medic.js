de14301b18d5dcf38ffbdadabc6010bde86e3298
Vladimir Volodin <zgollum@gmail.com>
2012-12-26 15:55:50 +0200

    SignalR files

---
+++ PilotProject/PilotProject/Scripts/medic.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    var messages = $.connection.messageHub;
+
+    messages.client.updateMedic = function (medic) {
+        console.log(medic);
+        $("#firstName").html(medic.firstName);
+        $("#lastName").html(medic.lastName);
+    }
+
+    $.connection.hub.start().done(function () {
+        messages.server.subToMedic($("#medicId").val());
+    });
+
+});
